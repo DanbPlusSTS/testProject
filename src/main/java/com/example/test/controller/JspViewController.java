fd6708686388265613392942aafee7286d7d3cc6
SongHwaJun <hwajun.song@danbplus.com>
2024-06-10 15:14:47 +0900

    뷰컨트롤러 원복

--- src/main/java/com/example/test/controller/JspViewController.java
+++ src/main/java/com/example/test/controller/JspViewController.java
@@ -1,13 +1,11 @@
 package com.example.test.controller;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
@@ -20,18 +18,17 @@ public class JspViewController {
     }
 	
 	@RequestMapping(value={"/javascriptTest"})
-    private ModelAndView javascriptTest(HttpServletRequest request, Model model) {
+    private ModelAndView main(HttpServletRequest request, Model model) {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("javascriptTest"); // 메인화면으로 이동
         return mv;
     }
 	
-	@RequestMapping(value={"/ajaxJavascript"})
-    private ModelAndView ajaxJavascript(HttpServletRequest request, Model model) {
+	@RequestMapping(value={"/ajaxJavascriptTest"})
+    private ModelAndView ajaxJavascriptTest(HttpServletRequest request, Model model) {
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("ajaxJavascript"); // 메인화면으로 이동
+        mv.setViewName("javascriptTest"); // 메인화면으로 이동
         return mv;
     }
-	
 	
 }
