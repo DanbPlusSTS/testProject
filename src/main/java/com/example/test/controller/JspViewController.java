501a015a95d17d1503ed1e14209ac992d2bf988b
SongHwaJun <hwajun.song@danbplus.com>
2024-06-10 15:39:25 +0900

    javascript getmapping 추가

--- src/main/java/com/example/test/controller/JspViewController.java
+++ src/main/java/com/example/test/controller/JspViewController.java
@@ -1,8 +1,12 @@
 package com.example.test.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -30,5 +34,20 @@ public class JspViewController {
         mv.setViewName("ajaxJavascriptTest"); // 메인화면으로 이동
         return mv;
     }
+	
+	@GetMapping("/ajaxJavascript")
+	  public Map<String,Object> ajaxJavascript(@RequestParam String input){
+	    Map<String, Integer> test = new HashMap<>();
+	    test.put("국어", 85);
+	    test.put("수학", 70);
+	    test.put("영어", 86);
+	        
+	    Map<String, Object> returnMap = new HashMap<>();
+	    returnMap.put("과목", input);
+	    returnMap.put("점수", test.get(input));
+	        
+	    return returnMap;
+	}
+	    
 	
 }
