23fad890669caf7ac9046b45ee7dda92bb98927a
SongHwaJun <hwajun.song@danbplus.com>
2024-06-10 14:34:50 +0900

    ajaxJavascript get 추가

--- src/main/java/com/example/test/controller/JspViewController.java
+++ src/main/java/com/example/test/controller/JspViewController.java
@@ -1,11 +1,13 @@
 package com.example.test.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
@@ -18,10 +20,26 @@ public class JspViewController {
     }
 	
 	@RequestMapping(value={"/javascriptTest"})
-    private ModelAndView main(HttpServletRequest request, Model model) {
+    private ModelAndView javascriptTest(HttpServletRequest request, Model model) {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("javascriptTest"); // 메인화면으로 이동
         return mv;
     }
+	
+	@GetMapping(value={"/ajaxJavascript"})
+      public Map<String,Object> ajaxJavascript(@RequestParam String input){
+	    Map<String, Integer> test = new HashMap<>();
+	    test.put("국어", 85);
+	    test.put("수학", 70);
+	    test.put("영어", 86);
+	    
+	    Map<String, Object> returnMap = new HashMap<>();
+	    returnMap.put("과목", input);
+	    returnMap.put("점수", test.get(input));
+	    
+        return returnMap;
+	  
+	}
+	
 	
 }
