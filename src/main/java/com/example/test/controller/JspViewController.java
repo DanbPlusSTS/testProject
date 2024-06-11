78a4c0dddad61a94def0e4744d4523d8da79bd10
SongHwaJun <hwajun.song@danbplus.com>
2024-06-11 16:58:09 +0900

    javascript POST타입 추가

--- src/main/java/com/example/test/controller/JspViewController.java
+++ src/main/java/com/example/test/controller/JspViewController.java
@@ -2,14 +2,15 @@ package com.example.test.controller;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
-
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
@@ -20,6 +21,7 @@ public class JspViewController {
 		mv.setViewName("jqueryTest"); // 메인화면으로 이동
 		return mv;
     }
+	
 	
 	@RequestMapping(value={"/javascriptTest"})
     private ModelAndView main(HttpServletRequest request, Model model) {
@@ -35,19 +37,53 @@ public class JspViewController {
         return mv;
     }
 	
-	@GetMapping("/ajaxJavascriptTest")
-	  public Map<String,Object> ajaxJavascript(@RequestParam(value="kwamok", required=false) String kwamok){
-	    Map<String, Integer> test = new HashMap<>();
-	    test.put("국어", 85);
-	    test.put("수학", 70);
-	    test.put("영어", 86);
+	@SuppressWarnings("unchecked")
+	@GetMapping("/ajaxJavascriptAction")
+	@ResponseBody
+	public JSONObject getAjaxJavascript(@RequestParam(value="kwamok", required=false) String kwamok){
+	    JSONObject resultObj = new JSONObject(); 
+	    resultObj.put("국어", 85);
+	    resultObj.put("수학", 70);
+	    resultObj.put("영어", 86);
+	    //System.out.println("1 : " + resultObj);
+
+	    JSONObject returnObj = new JSONObject();
+	    returnObj.put("subject", kwamok);
+	    returnObj.put("entry", resultObj.get(kwamok));
+	    //System.out.println("2 : " + returnObj);
 	        
-	    Map<String, Object> returnMap = new HashMap<>();
-	    returnMap.put("subject", kwamok);
-	    returnMap.put("entry", test.get(kwamok));
-	        
-	    return returnMap;
-	}
-	    
+	    return returnObj;
+	} 
+	
+	@SuppressWarnings("unchecked")
+    @PostMapping("/ajaxJavascriptAction2")
+	@ResponseBody
+    public JSONObject postAjaxJavascript(@RequestParam(value="kwamok", required=false) String kwamok){
+        JSONObject resultObj = new JSONObject();  
+        resultObj.put("국어", 85);
+        resultObj.put("수학", 70);
+        resultObj.put("영어", 86);
+        //System.out.println("1 : " + resultObj);
+
+        JSONObject returnObj = new JSONObject();
+        returnObj.put("subject", kwamok);
+        returnObj.put("entry", resultObj.get(kwamok));
+        System.out.println("2 : " + returnObj);
+            
+        return returnObj;
+    }
+	
+	
+    @RequestMapping(value={"/ajaxJqueryTest"})
+    private ModelAndView ajaxJqueryTest(HttpServletRequest request, Model model) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("ajaxJqueryTest"); // 메인화면으로 이동
+        return mv;
+    }
+	
+	
+	
+	
+	
 	
 }
