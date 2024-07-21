17d994e0c01ec4f14cd9821e4ce642658f1aa5c4
박신호 <@192.168.123.103>
2024-07-22 01:24:46 +0900

    test 소스 추가

---
+++ src/test/java/com/example/test/sample/DataConvertTest.java
@@ -0,0 +1,16 @@
+package com.example.test.sample;
+
+import org.junit.jupiter.api.Test;
+
+class DataConvertTest {
+
+	@Test
+	void dataConvertSample() {
+		/* 
+		 Map 데이터를 JSON으로 변환하는 샘플
+		 JSON데이터를 Map 데이터로 변환하는 샘플
+		 JSONArray 데이터 샘플
+		 Map 데이터 2개를 JSONArray로 변환하는 샘플
+		*/
+	}
+}
