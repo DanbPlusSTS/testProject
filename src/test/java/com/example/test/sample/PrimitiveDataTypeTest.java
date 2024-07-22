cef24bf046f15fcfd62c74289d28e4aab520c4b3
SongHwaJun <hwajun.song@danbplus.com>
2024-07-22 16:53:29 +0900

    기본 자료 변수형

--- src/test/java/com/example/test/sample/PrimitiveDataTypeTest.java
+++ src/test/java/com/example/test/sample/PrimitiveDataTypeTest.java
@@ -8,5 +8,38 @@ class PrimitiveDataTypeTest {
 	void primitiveDataType() {
 		//기본 자료형 변수 샘플 작성후 로그로 출력해보기.
 		//로그는 System.out.println 로그로 출력하세요.
+	  
+	  //byte
+	  byte a = 1;
+	  System.out.println("byte : " + a);
+	  
+	  //short
+	  short b = 22222;
+	  System.out.println("short : " + b);
+	  
+	  //int
+	  int c = 333333333;
+	  System.out.println("int : " + c);
+	  
+	  //long
+	  long d = 4444444444444444444L;
+	  System.out.println("long : " + d);
+	  
+	  //float
+	  float e = 5.555555f;
+	  System.out.println("float : " + e);
+	  
+	  //double
+	  double f = 6.666666666666666;
+	  System.out.println("double : " + f);
+	  
+	  //char
+	  char g = 'A';
+	  System.out.println("char : " + g);
+	  
+	  //boolean
+	  boolean h = true;
+	  System.out.println("boolean : " + h);
+	  
 	}
 }
