8f4f30eab2bd06ccd2ac2d57b831fc765e791b0f
SongHwaJun <hwajun.song@danbplus.com>
2024-07-23 16:55:03 +0900

    변수형의 SIZE 출력

--- src/test/java/com/example/test/sample/PrimitiveDataTypeTest.java
+++ src/test/java/com/example/test/sample/PrimitiveDataTypeTest.java
@@ -1,5 +1,6 @@
 package com.example.test.sample;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class PrimitiveDataTypeTest {
@@ -12,34 +13,42 @@ class PrimitiveDataTypeTest {
 	  //byte
 	  byte a = 1;
 	  System.out.println("byte : " + a);
+	  System.out.println("byte 크기 : " + Byte.SIZE);
 	  
 	  //short
 	  short b = 22222;
 	  System.out.println("short : " + b);
+	  System.out.println("short 크기 : " + Short.SIZE);
 	  
 	  //int
 	  int c = 333333333;
 	  System.out.println("int : " + c);
+	  System.out.println("int 크기 : " + Integer.SIZE);
 	  
 	  //long
 	  long d = 4444444444444444444L;
 	  System.out.println("long : " + d);
+	  System.out.println("long 크기 : " + Long.SIZE);
 	  
 	  //float
 	  float e = 5.555555f;
 	  System.out.println("float : " + e);
+	  System.out.println("float 크기 : " + Float.SIZE);
 	  
 	  //double
 	  double f = 6.666666666666666;
 	  System.out.println("double : " + f);
+	  System.out.println("double 크기 : " + Double.SIZE);
 	  
 	  //char
 	  char g = 'A';
 	  System.out.println("char : " + g);
+	  System.out.println("char 크기 : " + Character.SIZE);
 	  
 	  //boolean
 	  boolean h = true;
 	  System.out.println("boolean : " + h);
+	  System.out.println("boolean 크기는 따로 구할 수 없음 ");
 	  
 	}
 }
